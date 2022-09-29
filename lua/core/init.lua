1a04acb08dca8845001cc67dbc403d4b65035ac1
yili.zhang <andyili3947@gmail.com>
2022-09-28 17:41:28 -0700

    change tabstop to 2

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -13,9 +13,9 @@ if (vim.fn.has('termguicolors') == 1) then
 end
 -- tabs
 vim.opt.autoindent  = true
-vim.opt.tabstop     = 4
-vim.opt.shiftwidth  = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop     = 2
+vim.opt.shiftwidth  = 2
+vim.opt.softtabstop = 2
 vim.opt.mouse       = 'a'
 vim.opt.expandtab   = true
 vim.opt.autowrite   = false
