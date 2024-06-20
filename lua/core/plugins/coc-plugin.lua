7f5d41db50249004bd26ab6a488d8419423ce13a
plumage3947 <andyili3947@gmail.com>
2024-06-20 14:32:01 -0700

    Comment out coc

--- lua/core/plugins/coc-plugin.lua
+++ lua/core/plugins/coc-plugin.lua
@@ -1,6 +1,6 @@
-return {
-    {
-        "neoclide/coc.nvim",
-        branch = "release"
-    }
-}                 
+-- return {
+--     {
+--         "neoclide/coc.nvim",
+--         branch = "release"
+--     }
+-- }                 
